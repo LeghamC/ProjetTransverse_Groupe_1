e527f123721966936aaef25ffde51f54cd04de6a
LeghamC <151147032+leghamc@users.noreply.github.com>
2024-01-26 15:20:35 +0100

    Creation of the main file

---
+++ main.py
@@ -0,0 +1,12 @@
+# -------------------------------------------------------------------------------
+# Name:        Clocklock
+# Author:      Lélia - Dali - Meïssa - Manon
+# Purpose:     Project's main program
+# Created:     01/02/2024
+# -------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    # import of the functions created
+
+    """ """
